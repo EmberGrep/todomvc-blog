d3ce7146980a49eb35ebe97b33e300b0a265bf68
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-22 19:05:30 -0600

    Update styles

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    emberCliFontAwesome: {
+      useScss: true
+    }
   });
 
   app.import('bower_components/highlightjs/styles/github.css');
